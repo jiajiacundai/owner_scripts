d80ccf26807c348d314054645ec57a81038fb921
jiajiacundai <109328137+jiajiacundai@users.noreply.github.com>
2024-04-27 17:54:04 +0800

    Update up_yum.py

--- up_yum.py
+++ up_yum.py
@@ -10,20 +10,20 @@ for filename in os.listdir(directory):
     # if 'yum' in filename:
     if 'yum' == filename or "yum-config-manager" == filename:
         file_path = os.path.join(directory, filename)
-        with open(file_path, 'r', encoding="us-ascii") as file:
+        with open(file_path, 'r') as file:
             lines = file.readlines()
             if len(lines) > 0 and not lines[0].startswith('#!/usr/bin/python2.7'):
                 lines[0] = lines[0].replace("python", "python2.7")
-                with open(file_path, 'w', encoding="us-ascii") as modified_file:
+                with open(file_path, 'w') as modified_file:
                     modified_file.writelines(lines)
                 print('已修改的文件：{}'.format(filename))
 
 # 修改配置文件
 def change_selinux_mode(mode):
     config_file_path = "/etc/selinux/config"
-    sed_command = f"sed -i 's/^SELINUX=.*/SELINUX={mode}/' {config_file_path}"
+    sed_command = "sed -i 's/^SELINUX=.*/SELINUX={}/' {}".format(mode, config_file_path)
     subprocess.run(sed_command, shell=True, check=True)
-    print(f"SELinux mode has been changed to {mode}")
+    print('SELinux mode has been changed to {}'.format(mode))
 
 # 将SELinux模式设置为permissive
 change_selinux_mode("permissive")
@@ -31,14 +31,14 @@ change_selinux_mode("permissive")
 # 修改指定文件的第一行
 file_path = '/usr/libexec/urlgrabber-ext-down'
 
-with open(file_path, 'r', encoding="us-ascii") as file:
+with open(file_path, 'r') as file:
     lines = file.readlines()
 
 modified = False
 
 if len(lines) > 0 and not lines[0].startswith('#! /usr/bin/python2.7'):
     lines[0] = lines[0].replace("python", "python2.7")
-    with open(file_path, 'w', encoding="us-ascii") as modified_file:
+    with open(file_path, 'w') as modified_file:
         modified_file.writelines(lines)
     modified = True
 
