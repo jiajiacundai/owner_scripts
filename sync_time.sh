8792ac74259801caed618733d7b1fb566640e322
jiajiacundai <109328137+jiajiacundai@users.noreply.github.com>
2024-10-12 19:03:23 +0800

    Update sync_time.sh

--- sync_time.sh
+++ sync_time.sh
@@ -6,10 +6,19 @@ if [ -z "$TIME" ]; then
     exit 1
 fi
 
+# 检查当前时区是否为上海
+CURRENT_TIMEZONE=$(timedatectl | grep "Time zone" | awk '{print $3}')
+if [ "$CURRENT_TIMEZONE" != "Asia/Shanghai" ]; then
+    echo "当前时区为 $CURRENT_TIMEZONE，设置时区为 Asia/Shanghai"
+    timedatectl set-timezone Asia/Shanghai
+else
+    echo "当前时区已为 Asia/Shanghai"
+fi
+
 # 将ISO 8601格式的时间转换为date可识别的格式
 FORMATTED_TIME=$(date -d "$TIME" +"%Y-%m-%d %H:%M:%S")
 
 # 使用date命令设置系统时间
-sudo date -s "$FORMATTED_TIME"
+date -s "$FORMATTED_TIME"
 
 echo "系统时间已同步为: $(date)"
