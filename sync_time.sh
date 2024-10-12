48bd01d7dab9a210998912e9125d7f15efe6924b
jiajiacundai <109328137+jiajiacundai@users.noreply.github.com>
2024-10-12 18:39:17 +0800

    Update sync_time.sh

--- sync_time.sh
+++ sync_time.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# 使用curl从世界时间API获取时间
+# 使用 curl 从世界时间API获取时间
 TIME=$(curl -s http://worldtimeapi.org/api/timezone/Etc/UTC | grep -o '"datetime":"[^"]*' | cut -d'"' -f4)
 
 if [ -z "$TIME" ]; then
@@ -8,10 +8,13 @@ if [ -z "$TIME" ]; then
     exit 1
 fi
 
-# 将ISO 8601格式的时间转换为date可识别的格式
-FORMATTED_TIME=$(date -d "$TIME" +"%Y%m%d%H%M.%S")
+# 显示获取到的ISO时间，调试用
+echo "获取到的UTC时间: $TIME"
 
-# 使用date命令设置系统时间
+# 将 ISO 8601 格式的时间转换为 date 可识别的格式（添加 -u 表示使用 UTC 时间）
+FORMATTED_TIME=$(date -u -d "$TIME" +"%Y%m%d%H%M.%S")
+
+# 使用 date 命令设置系统时间
 sudo date "$FORMATTED_TIME"
 
 echo "系统时间已同步为: $(date)"
